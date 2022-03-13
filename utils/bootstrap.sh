6832c39b384e67cf24b7f4d51dcf30a22b1a2d8f
MilkyDeveloper <48262994+MilkyDeveloper@users.noreply.github.com>
2022-03-13 15:26:33 -0500

    Remove no-clobber (would cause issues in debugging)

--- utils/bootstrap.sh
+++ utils/bootstrap.sh
@@ -20,8 +20,8 @@ function bootstrapFiles {
 
   # Download the kernel bzImage and the kernel modules (wget)
   {
-  wget https://github.com/cb-linux/breath/releases/latest/download/bzImage -nc -O bzImage -q --show-progress
-  wget https://github.com/cb-linux/breath/releases/latest/download/modules.tar.xz -nc -O modules.tar.xz -q --show-progress
+  wget https://github.com/cb-linux/breath/releases/latest/download/bzImage -O bzImage -q --show-progress
+  wget https://github.com/cb-linux/breath/releases/latest/download/modules.tar.xz -O modules.tar.xz -q --show-progress
   wget https://raw.githubusercontent.com/cb-linux/kernel/main/kernel.flags -O kernel.flags -q --show-progress
   } || true # Wget has the wrong exit status with no clobber
 
