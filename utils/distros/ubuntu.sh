204801c24dd351aa86a5ff010bfba30c1fad1d88
advapp <IronRod3@protonmail.com>
2022-03-17 10:47:14 -0700

    Removed test printf

--- utils/distros/ubuntu.sh
+++ utils/distros/ubuntu.sh
@@ -7,12 +7,11 @@ function postinstall {
     # Determine server based upon build system locale
     TEMP=$(locale | grep LANG=)
     LANG=${TEMP:5:-1}
-    printf "\>${LANG}\<\n"
-
+    
     # Get the two-character region and set to lower-case
     LOC=${LANG:3:2}
     PREFIX=${LOC,,}
-    
+
     # Designate main server for the region
     SOURCE="deb http://${PREFIX}.archive.ubuntu.com/ubuntu ${DISTRO_CODENAME}"
     printq "${SOURCE}"
