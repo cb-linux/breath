87959aefa5803e5bf56dd37423f3209b74f34ab5
timmypidashev <pidashev.tim@gmail.com>
2022-05-01 18:46:22 -0700

    Strip unneeded work in accordance with discussion

--- __init__.py
+++ __init__.py
@@ -11,15 +11,10 @@ __license__ = 'MIT'
 __copyright__ = 'Copyright 2021-present MilkyDeveloper'
 __version__ = '4.1.1-port/python'
 
+import subprocess
 import traceback
 import argparse
 import sys
-
-from .interactions import *
-from .functions import *
-from .installer import *
-from .system import *
-from .errors import *
 
 # The default configuration options for Breath.
 # Used for comparison between user options and defaults
@@ -42,7 +37,6 @@ defaults = {
     'version': False
 }
 
-user_input = dict() # This dict will hold all the install options for the breath installer to use
 
 class CustomHelpFormatter(argparse.HelpFormatter):
     """
@@ -72,7 +66,7 @@ def define_and_parse_args():
     parser = argparse.ArgumentParser(
         prog=__title__,
         usage='&(prog)s [options...]',
-        description=cli_format(),
+        description=__summary__,
         formatter_class=fmt
     )
 
@@ -86,16 +80,14 @@ def define_and_parse_args():
     parser.add_argument('-c', '--crostini', default=defaults['crostini'], help='installer using a chrome-based system (default: %(default)s)', action='store_true')
     parser.add_argument('-lk', '--local_kernel', default=defaults['local_kernel'], help='use a local build of the kernel bzImage (default: %(default)s)', action='store_true')
     parser.add_argument('-f', '--force_defaults', default=defaults['force_defaults'], help='install with default configuration (see defaults)', action='store_true')
-    parser.add_argument('-cl', '--cleanup', default=defaults['cleanup'], help='remove build files after install (default: %(default)s)', action='store_true')
-    parser.add_argument('-vv', '--verbose', default=defaults['verbose'], help='set error output to verbose (default: %(default)s)', action='store_true')
     parser.add_argument('-vd', '--view_defaults', default=defaults['view_defaults'], help='view default configuration options', action='store_true')
     parser.add_argument('-v', '--version', default=defaults['version'], help='output version information and exit', action='store_true')
 
     # Parse args
     args = parser.parse_args()
 
-    # Update argparse.Namespace() contents to dict(installation_options)
-    user_input.update(vars(args))
+    # Add argparse options to dict(user_input)
+    user_input = (vars(args))
 
     if args.view_defaults:
         info(defaults)
@@ -105,6 +97,12 @@ def define_and_parse_args():
         info(f'{__title__} v{__version__}')
         sys.exit()
 
+    else:
+        print(parser.print_help())
+        sys.exit()
+
+    return user_input
+
 
 def run_as_a_module():
     """
@@ -113,12 +111,9 @@ def run_as_a_module():
     and the file '__main__.py' acts as a entry point.
 	"""
 
-    # Breath start dir
-    original_dir = os.getcwd()
-
     # Argparse command-line parsers
     # NOTE: Runs only if ran as a module
-    define_and_parse_args()
+    options = define_and_parse_args()
 
     try:
         # Exit if running as root.
@@ -126,51 +121,7 @@ def run_as_a_module():
             sys.tracebacklimit = 0 # set as options is not yet defined
             raise DontRunAsRoot('Please do not run Breath as root!')
 
-        # If the system and/or distribution is supported, but the user
-        # did not specify any flags, assume the user needs to set everything.
-        # This brings up installation inputs, after which installation proceeds with set flags. 
-        # NOTE: If --forcedefaults is set, then default args are used.
-        options = BreathInquirer(defaults, user_input, __version__)
-
-        # Set verbosity level of traceback.
-        if options['verbose'] == False:
-            sys.tracebacklimit = 0
-
-        # Define host system the Breath installer is running on.
-        system = BreathSystem()
-
-        # Update host system packages.
-        system.update_packages()
-
-        # Install dependencies onto host system.
-        system.install_dependencies(['vboot-kernel-utils', 'arch-install-scripts', 'git', 'wget', 'cgpt'])
-
-        # Define and run BreathInstaller().
-        installer = BreathInstaller()
-
-        # Before we do anything, check if the user is on Crostini,
-        # since /mnt will already be filled.
-        mnt = installer.check_crostini(options['crostini'])
-
-        # Bootstrap files.
-        build_dir = installer.bootstrap_files(options['local_kernel'], options['distro'], mnt, original_dir)
-
-        # Configure install_type.
-        # NOTE: # If the user wants to build an ISO, the partition numbers are different,
-        # (e.g.) /dev/sda2 or /dev/loop9p2 (notice the "p").
-        # partitions = installer.configure_install_type(options['install_type'])
-
-        # Clean up after installation.
-        # installer.cleanup()
-
-    except BreathException:
-        """
-        Print exceptions defined in Breath if caught.
-        NOTE: sys.exit() is used to prevent python from
-        Catching BreathException as Exception.
-        """
-        error(traceback.format_exc())
-        sys.exit()
+        # Run core installer here and pass options
 
     except Exception:
         """
@@ -178,10 +129,4 @@ def run_as_a_module():
         NOTE: Verbosity levels ignored since an exception
         thrown by python is usually a bug.
         """
-        fatal(traceback.format_exc())
-
-
-
-
-
-
+        sys.exit()
