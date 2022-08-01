25cbf6fece6477545bdc6701889813db24d32a8a
Timothy Pidashev <pidashev.tim@gmail.com>
2022-08-01 16:03:27 -0700

    Initial python structure

---
+++ ports/python/__init__.py
@@ -0,0 +1,4 @@
+import breath
+
+if __name__ == '__main__':
+    breath.run_as_module()
---
+++ ports/python/__main__.py
@@ -0,0 +1,17 @@
+#-*- encoding: utf-8 -*-
+
+"""
+Python Breath adaptation
+"""
+
+__title__ = 'breath'
+__summary__ = 'A way to natively run Linux on modern Chromebooks without replacing firmware.'
+__author__ = 'MilkyDeveloper'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021-present MilkyDeveloper'
+__version__ = '4.2.2-feature/python-base'
+
+
+def run_as_module():
+    pass
+    
