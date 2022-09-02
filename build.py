bd789ef60fcf9ec3e87f67877aeb7bfa00710520
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-09-02 02:55:15 +0000

    Add files via upload

---
+++ build.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python3
+
+import os
+from shutil import rmtree as rmdir
+from pathlib import Path
+import sys
+import argparse
+import urllib.request
+import subprocess as sp
+from os import system as bash
+import user_input
+import urllib.error
+
+
+# parse arguments from the cli. Only for testing/advanced use. 95% of the arguments are handled by the user_input script
+def process_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--local-path', dest="local_path",
+                        help="Local path for kernel files, to use instead of downloading from github." +
+                             "(Unsigned kernels only)")
+    parser.add_argument("--dev", action="store_true", dest="dev_build", default=False,
+                        help="Use latest dev build. May be unstable.")
+    parser.add_argument("--alt", action="store_true", dest="alt", default=False,
+                        help="Use alt kernel. Only for older devices.")
+    parser.add_argument("--exp", action="store_true", dest="exp", default=False,
+                        help="Use experimental 5.15 kernel.")
+    return parser.parse_args()
+
+
+# Clean /tmp from eupnea files
+def prepare_host(de_name) -> None:
+    print("\033[96m" + "Preparing host system" + "\033[0m")
+    print("Creating /tmp/eupnea-build")
+    bash("umount -f /tmp/eupnea-build/arch")
+    rmdir("/tmp/eupnea-build", ignore_errors=True)
+    Path("/tmp/eupnea-build/rootfs").mkdir(parents=True)
+    print("Creating mnt point")
+    bash("umount -lf /mnt/eupnea")  # just in case
+    rmdir("/mnt/eupnea", ignore_errors=True)
+    Path("/mnt/eupnea").mkdir(parents=True, exist_ok=True)
+    print("Installing necessary packages")
+    # apt only for now
+    # install cgpt and futility
+    if sp.run("apt install cgpt vboot-kernel-utils", shell=True, capture_output=True).stdout.decode(
+            "utf-8").strip() == "/bin/sh: 1: apt: not found":
+        print("\033[91m" + "cgpt and futility not found, please install them using your disotros package manager"
+              + "\033[0m")
+        exit()
+    if de_name == "debian":
+        if sp.run("apt install debootstrap", shell=True, capture_output=True).stdout.decode(
+                "utf-8").strip() == "/bin/sh: 1: apt: not found":
+            print("\033[91m" + "Debootstrap not found, please install it using your disotros package manager"
+                  + "\033[0m")
+            exit()
+    elif de_name == "arch":
+        if sp.run("apt install arch-install-scripts", shell=True, capture_output=True).stdout.decode(
+                "utf-8").strip() == "/bin/sh: 1: apt: not found":
+            print("\033[91m" + "Debootstrap not found, please install it using your disotros package manager"
+                  + "\033[0m")
+            exit()
+
+
+# download kernel files from GitHub
+def download_kernel(dev_build: bool) -> None:
+    print("\033[96m" + "Downloading kernel binaries from the eupnea github" + "\033[0m")
+    if dev_build:
+        url = "https://github.com/eupnea-linux/eupnea/releases/download/dev-build/"
+    else:
+        url = "https://github.com/eupnea-linux/eupnea/releases/download/latest/"
+    try:
+        if args.alt:
+            print("Downloading alt kernel")
+            urllib.request.urlretrieve(url + "bzImage.alt", filename="/tmp/eupnea-build/bzImage")
+            urllib.request.urlretrieve(url + "modules.alt.tar.xz", "/tmp/eupnea-build/alt-kernel.tar.gz")
+        elif args.exp:
+            print("Downloading experimental 5.15 kernel")
+            urllib.request.urlretrieve(url + "bzImage.exp", filename="/tmp/eupnea-build/bzImage")
+            urllib.request.urlretrieve(url + "modules.exp.tar.xz", "/tmp/eupnea-build/exp-kernel.tar.gz")
+        else:
+            urllib.request.urlretrieve(url + "bzImage", filename="/tmp/eupnea-build/bzImage")
+            urllib.request.urlretrieve(url + "modules.tar.xz", filename="/tmp/eupnea-build/modules.tar.xz")
+    except urllib.error.URLError:
+        print("\033[91m" + "Failed to reach github. Check your internet connection and try again" + "\033[0m")
+        exit()
+
+
+# Prepare USB, usb is not yet fully implemented
+def prepare_usb() -> None:
+    print("\033[96m" + "Preparing USB" + "\033[0m")
+
+
+# Create, mount, partition the img and flash the eupnea kernel
+def prepare_img() -> str:
+    print("\033[96m" + "Preparing img" + "\033[0m")
+    print("Allocating space for image, might take a while")
+    # 12G for now, maybe decrease later
+    # try fallocate, if it fails use dd
+    if sp.run("fallocate -l 12G eupnea.img", shell=True, capture_output=True).stderr.decode(
+            "utf-8").strip() == "/bin/sh: 1: fallocate: not found":
+        bash("dd if=/dev/zero of=eupnea.img status=progress bs=12884 count=1000070")
+    print("Mounting empty image")
+    img_mnt = sp.run(["losetup", "-f", "--show", "eupnea.img"], capture_output=True).stdout.decode("utf-8").strip()
+    print("Image mounted at" + img_mnt)
+    # format usb as per deptcharge requirements
+    print("Partitioning mounted image and adding flags")
+    bash("parted -s " + img_mnt + " mklabel gpt")
+    bash("parted -s -a optimal " + img_mnt + " unit mib mkpart Kernel 1 65")
+    bash("parted -s -a optimal " + img_mnt + " unit mib mkpart Root 65 100%")
+    bash("cgpt add -i 1 -t kernel -S 1 -T 5 -P 15 " + img_mnt)
+    # get uuid of rootfs partition
+    usb_rootfs = sp.run(["blkid", "-o", "value", "-s", "PARTUUID", img_mnt + "p2"], capture_output=True).stdout.decode(
+        "utf-8").strip()
+    # read and modify kernel flags
+    with open("configs/kernel.flags", "r") as file:
+        temp = file.read().replace("${USB_ROOTFS}", usb_rootfs).strip()
+    with open("kernel.flags", "w") as file:
+        file.write(temp)
+    print("Signing kernel")
+    bash("futility vbutil_kernel --arch x86_64 --version 1 --keyblock /usr/share/vboot/devkeys/kernel.keyblock"
+         + " --signprivate /usr/share/vboot/devkeys/kernel_data_key.vbprivk --bootloader kernel.flags" +
+         " --config kernel.flags --vmlinuz /tmp/eupnea-build/bzImage --pack /tmp/eupnea-build/bzImage.signed")
+    print("Flashing kernel")
+    bash("dd if=/tmp/eupnea-build/bzImage.signed of=" + img_mnt + "p1")
+    print("Formating rootfs")
+    bash("yes | mkfs.ext4 " + img_mnt + "p2")
+    print("Mounting rootfs to /mnt/eupnea")
+    bash("mount " + img_mnt + "p2 /mnt/eupnea")
+    return img_mnt
+
+
+# download the distro rootfs
+def download_rootfs(distro_name: str, distro_version: str, distro_link: str) -> None:
+    print("\033[96m" + "Downlaoding rootfs. Do not panick if script seems stuck" + "\033[0m")
+    # TODO: Add progress bar or something
+    try:
+        match distro_name:
+            case "ubuntu":
+                print("Downloading ubuntu rootfs " + distro_version)
+                urllib.request.urlretrieve(
+                    "https://cloud-images.ubuntu.com/releases/" + distro_version + "/release/ubuntu-"
+                    + distro_version + "-server-cloudimg-amd64-root.tar.xz",
+                    filename="/tmp/eupnea-build/rootfs/ubuntu-rootfs.tar.xz")
+            case "debian":
+                print("Downloading debian with debootstrap")
+                bash("debootstrap stable /tmp/eupnea-build/rootfs https://deb.debian.org/debian/")
+                pass
+            case "arch":
+                print("Downloading latest arch rootfs")
+                urllib.request.urlretrieve(
+                    "https://mirror.rackspace.com/archlinux/iso/latest/archlinux-bootstrap-x86_64.tar.gz",
+                    filename="/tmp/eupnea-build/rootfs/arch-rootfs.tar.gz")
+            case "fedora":
+                print("Downloading fedora rootfs version: " + distro_version)
+                urllib.request.urlretrieve(distro_link, filename="/tmp/eupnea-build/rootfs/fedora-rootfs.tar.xz")
+    except urllib.error.URLError:
+        print(
+            "\033[91m" + "Failed to download rootfs. Check your internet connection and try again. If the error" +
+            " persists, create an issue with the distro and version in the name" + "\033[0m")
+        exit()
+
+
+# extract the rootfs to the img
+def extract_rootfs(distro: str) -> None:
+    print("\033[96m" + "Extracting rootfs" + "\033[0m")
+    match distro:
+        case "ubuntu":
+            print("Extracting ubuntu rootfs")
+            bash("tar xfp /tmp/eupnea-build/rootfs/ubuntu-rootfs.tar.xz -C " + "/mnt/eupnea --checkpoint=.10000")
+        case "debian":
+            print("Copying debian rootfs")
+            bash("cp -r -p /tmp/eupnea-build/rootfs/* /mnt/eupnea/")
+        case "arch":
+            print("Extracting arch rootfs")
+            # TODO: Figure out how to extract arch rootfs without cd
+            # temp chdir into mounted image, then go back to original dir
+            temp_path = os.getcwd()
+            os.chdir("/mnt/eupnea")
+            bash("tar xvpfz /tmp/eupnea-build/rootfs/arch-rootfs.tar.gz root.x86_64/ --strip-components=1" +
+                 " --numeric-owner")
+            os.chdir(temp_path)
+        case "fedora":
+            print("Extracting fedora rootfs to temporary location")
+            Path("/tmp/eupnea-build/fedora-temp").mkdir()
+            bash("tar xfp /tmp/eupnea-build/rootfs/fedora-rootfs.tar.xz -C /tmp/eupnea-build/rootfs/fedora-temp")
+            with os.scandir("/tmp/eupnea-build/rootfs/fedora-temp") as scan:
+                for entry in scan:
+                    if entry.is_dir():
+                        temp_rootfs_path = entry.path
+                        break
+            print("Copying fedora rootfs to /mnt/eupnea")
+            bash("tar xvpf " + temp_rootfs_path + "/layer.tar -C /mnt/eupnea")
+
+
+# Configure distro agnostic options
+def post_extract(username: str, password: str, hostname: str, rebind_search: bool) -> None:
+    print("\n\033[96m" + "Configuring Eupnea" + "\033[0m")
+    print("Copying resolv.conf")
+    bash("cp --remove-destination /etc/resolv.conf /mnt/eupnea/etc/resolv.conf")
+    # TODO: perhaps its not a good idea to cp firmware from host
+    print("Copying firmare from host")
+    Path("/mnt/eupnea/lib/firmware").mkdir(parents=True, exist_ok=True)
+    bash("cp -r /lib/firmware/* /mnt/eupnea/lib/firmware/")
+    print("Extracting kernel modules")
+    rmdir("/mnt/eupnea/lib/modules", ignore_errors=True)
+    Path("/mnt/eupnea/lib/modules").mkdir(parents=True, exist_ok=True)
+    bash("tar xpf /tmp/eupnea-build/modules.tar.xz -C /mnt/eupnea/")  # the tar contains /lib/modules already
+    print("Configuring user")
+    if not chroot("id " + username).find("no such user") == -1:
+        chroot('adduser --disabled-password --gecos "" ' + username)
+        chroot('echo "' + username + ':' + password + '" | chpasswd')
+        chroot("usermod -aG sudo " + username)
+    print("Setting hostname")
+    with open("/mnt/eupnea/etc/hostname", "w") as hostname_file:
+        hostname_file.write(hostname)
+    print("Copying eupnea utils")
+    # TODO: add py utils when rewrite is finished
+    bash("cp bin/* /mnt/eupnea/usr/local/bin/")
+    chroot("chmod 755 /mnt/eupnea/usr/local/bin/*")
+    print("Backing up default keymap and setting Chromebook layout")
+    chroot("cp /mnt/eupnea/usr/share/X11/xkb/symbols/pc /mnt/eupnea/usr/share/X11/xkb/symbols/pc.default")
+    chroot("cp -f xkb/xkb.chromebook /mnt/eupnea/usr/share/X11/xkb/symbols/pc")
+    if rebind_search:
+        print("Rebinding search key to Caps Lock")
+        chroot("cp /mnt/eupnea/usr/share/X11/xkb/keycodes/evdev /mnt/eupnea/usr/share/X11/xkb/keycodes/evdev.default")
+        chroot("cp xkb/xkb.chromebook /mnt/eupnea/usr/share/X11/xkb/symbols/pc")
+    print("Configuring sleep")
+    # disable deep sleep and hibernation
+    # TODO: Fix sleep?
+    Path("/mnt/eupnea/etc/systemd/").mkdir(exist_ok=True)
+    with open("/mnt/eupnea/etc/systemd/sleep.conf", "a") as file:
+        file.write("SuspendState=freeze\nHibernateState=freeze")
+    # open kernel-modules.txt and then append its contents to the rootfs file
+    with open("configs/kernel-modules.txt", "r") as repo_file:
+        with open("/mnt/eupnea/etc/modules", "a") as file:
+            file.write(repo_file.read())
+
+
+def chroot(command: str) -> str:
+    return sp.run('chroot /mnt/eupnea /bin/sh -c "' + command + '"', shell=True, capture_output=True).stdout.decode(
+        "utf-8").strip()
+
+
+if __name__ == "__main__":
+    # Elevate script to root
+    if os.geteuid() != 0:
+        args = ['sudo', sys.executable] + sys.argv + [os.environ]
+        os.execlpe('sudo', *args)
+    args = process_args()
+    if args.dev_build:
+        print("\033[93m" + "Using dev kernel" + "\033[0m")
+    user_input = user_input.user_input()
+    prepare_host(user_input[0])
+    if args.local_path is None:
+        download_kernel(args.dev_build)
+        kernel_path = "/tmp/eupnea-build/"
+    else:
+        if not args.local_path.endswith("/"):
+            kernel_path = args.local_path + "/"
+        else:
+            kernel_path = args.local_path
+    if user_input[9]:
+        img_mnt = prepare_img()
+    else:
+        prepare_usb()
+    download_rootfs(user_input[0], user_input[1], user_input[2])
+    extract_rootfs(user_input[0])
+    post_extract(user_input[5], user_input[6], user_input[7], user_input[8])
+    match user_input[0]:
+        case "ubuntu":
+            import distro.ubuntu as distro
+        case "debian":
+            import distro.debian as distro
+        case "arch":
+            import distro.arch as distro
+        case "fedora":
+            import distro.fedora as distro
+        case _:
+            print("\033[91m" + "Something went **really** wrong somewhere!(Distro name not found)" + "\033[0m")
+    distro.config(user_input[3])
+    print("\033[96m" + "Finishing setup" + "\033[0m")
+    print("Unmounting rootfs")
+    bash("umount /mnt/eupnea")
+    if user_input[9]:
+        print("Unmounting img")
+        bash("losetup -d " + img_mnt)
+        print("\033[95m" + "The ready Eupnea image is located at " + str(os.getcwd()) + "/eupnea.img" + "\033[0m")
+    else:
+        print("\033[95m" + "The USB is ready to boot Eupnea. " + "\033[0m")
---
+++ user_input.py
@@ -0,0 +1,217 @@
+from typing import Tuple
+import json
+from getpass import getpass
+
+
+def user_input() -> Tuple[str, str, str, str, bool, str, str, str, bool, bool]:
+    print("\033[95m" + "Welcome to Eupnea" + "\033[0m")
+    print("\033[95m" + "This script will help you create a bootable USB Stick/SD card for Eupnea" + "\033[0m")
+    print("\033[95m" + "Please answer the following questions." "\033[0m")
+    print("(If you dont know what to answer, just press enter and the recommended answer will be used)")
+    print("\033[92m" + "Which Linux distro(flavor) would you like to use?" + "\033[0m")
+    # setting optional vars to 0
+    distro_version = 0
+    use_openbox = False
+    rebind_search = False
+    create_iso = True
+    distro_link = ""
+    while True:
+        distro_name = input(
+            "\033[94m" + "Available options: Ubuntu(default, recommended), Debian, Arch, Fedora(cli only)\n" +
+            "\033[0m")
+        match distro_name:
+            case "Ubuntu" | "ubuntu" | "":
+                print("Ubuntu selected")
+                distro_name = "ubuntu"
+                print("\033[92mUse latest version?\033[0m")
+                temp_input = input("\033[94mPress enter for yes, or type in the version number(for example: '21.10'):\n"
+                                   + "\033[0m")
+                if temp_input == "":
+                    distro_version = "22.04"
+                    distro_link = "jammy"
+                    print("Using latest version: " + distro_version)
+                    break
+                else:
+                    with open("distros.json", "r") as file:
+                        distros = json.load(file)
+                    if temp_input in distros["ubuntu"]:
+                        distro_version = temp_input
+                        distro_link = distros["ubuntu"][distro_version]
+                        print("Using version: " + distro_version + " codename: " + distros["ubuntu"][temp_input])
+                        break
+                    else:
+                        print("\033[93m" + "Version not available, please choose another" + "\033[0m")
+                        continue
+            case "Debian" | "debian":
+                print("Debian selected")
+                break
+            case "Arch" | "arch" | "arch btw":
+                print("Arch selected")
+                break
+            case "Fedora" | "fedora":
+                print("Fedora selected")
+                if input("\033[93mWarning: No desktop environment available for Fedora" + "\033[94m" +
+                         "\nType 'yes' to continue or Press Enter to choose another distro(" + "\033[0m\n") == "yes":
+                    print("Fedora confirmed")
+                    temp_input = input("\033[92mUse latest version?" + "\033[94m" + "\nPress enter for yes, " +
+                                       "or type in the version number(for example: '35'). " +
+                                       "Versions 32 and 33 are not available:\n" + "\033[0m")
+                    if temp_input == "":
+                        distro_version = "36"
+                        print("Using version: " + distro_version)
+                        break
+                    else:
+                        with open("distros.json", "r") as file:
+                            distros = json.load(file)
+                        if temp_input in distros["fedora"]:
+                            distro_version = temp_input
+                            print("Using version: " + distro_version)
+                            break
+                        else:
+                            print("\033[93m" + "Version not available, please choose another" + "\033[0m")
+                            continue
+                else:
+                    print("\033[93mFedora not confirmed, please select another distro" + "\033[0m")
+                    continue
+            case _:
+                print("\033[93mCheck your spelling and try again" + "\033[0m")
+                continue
+    # TODO: add per distro selections
+    print("\033[92m" + "Which desktop environment would you like to use?" + "\033[0m")
+    while True:
+        de_name = input("\033[94mAvailable options: Gnome(default, recommended), KDE(recommended), MATE," +
+                        " Xfce(recommended for weak devices), LXQt(recommended for weak devices), deepin, budgie, " +
+                        "minimal, cli" + "\033[0m" + "\n")
+        match de_name:
+            case "Gnome" | "gnome" | "":
+                print("Gnome selected")
+                de_name = "gnome"
+                # if input("\033[92m" + "Replace default window manager with OpenBox?(NOT RECOMMENDED)" + "\033[94m"
+                #          + "\nType 'yes'. Press enter to use the default window manager instead.\033[0m\n") == "yes":
+                #     print("Openbox + Gnome selected")
+                #     use_openbox = True
+                # else:
+                #     print("Using the default window manager")
+                #     use_openbox = False
+                break
+            case "KDE" | "kde":
+                print("KDE selected")
+                de_name = "kde"
+                # if input("\033[92m" + "Replace default window manager with OpenBox?(NOT RECOMMENDED)" + "\033[94m"
+                #          + "\nType 'yes'. Press enter to use the default window manager instead.\033[0m\n") == "yes":
+                #     print("Openbox + KDE selected")
+                #     use_openbox = True
+                # else:
+                #     print("Using the default window manager")
+                #     use_openbox = False
+                break
+            case "MATE" | "mate":
+                print("MATE selected")
+                de_name = "mate"
+                break
+            case "xfce" | "Xfce":
+                print("Xfce selected")
+                de_name = "xfce"
+                break
+            case "lxqt" | "Lxqt":
+                print("Lxqt selected")
+                de_name = "lxqt"
+                break
+            case "deepin":
+                print("Deepin selected")
+                de_name = "deepin"
+                break
+            case "budgie":
+                print("Budgie selected")
+                de_name = "budgie"
+                break
+            case "minimal":
+                print("Minimal selected")
+                de_name = "minimal"
+                break
+            case "cli" | "none":
+                print("CLI selected")
+                if input("\033[93mWarning: No desktop environment will be installed" + "\033[94m\nType 'yes' to " +
+                         "continue or Press Enter to choose a desktop environment" + "\033[0m\n") == "yes":
+                    print("No desktop will be installed")
+                    de_name = "cli"
+                    break
+            case _:
+                print("\033[93m" + "Check your spelling and try again" + "\033[0m")
+    # Ubuntu + gnome now has a first time setup, so skip this part for ubuntu with gnome
+    if not distro_name == "ubuntu" and not de_name == "gnome":
+        print("\033[92m" + "Enter username to be used in Eupnea" + "\033[0m")
+        while True:
+            username = input("\033[94m" + "Username(default: 'localuser'): " + "\033[0m")
+            for char in username:
+                if char not in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._-":
+                    print("\033[93m" + "Username contains invalid character: " + char + "\033[0m")
+                    break
+            if username == "":
+                print("Using localuser as username")
+                username = "localuser"
+                break
+            else:
+                break
+
+        print("\033[92m" + "Please set a secure password" + "\033[0m")
+        while True:
+            passwd_temp = getpass("\033[94m" + "Password: " + "\033[0m")
+            if passwd_temp == "":
+                print("\033[93m" + "Password cannot be empty" + "\033[0m")
+                continue
+            else:
+                passwd_temp_2 = getpass("\033[94m" + "Repeat password: " + "\033[0m")
+                if passwd_temp == passwd_temp_2:
+                    password = passwd_temp
+                    print("Password set")
+                    break
+                else:
+                    print("\033[93m" + "Passwords do not match, try again" + "\033[0m")
+                    continue
+
+    print("\033[92m" + "Enter hostname for the chromebook.(Hostname is something like a device name)" + "\033[0m")
+    while True:
+        hostname = input("\033[94m" + "Hostname(default: 'eupnea-chromebook'): " + "\033[0m")
+        if hostname == "":
+            print("Using eupnea-chromebook as hostname")
+            hostname = "eupnea-chromebook"
+            break
+        if hostname[0] == "-":
+            print("\033[93m" + "Hostname cannot start with a '-'" + "\033[0m")
+            continue
+        for char in hostname:
+            if char not in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._-":
+                print("\033[93m" + "Hostname contains invalid character: " + char + "\033[0m")
+                break
+        else:
+            break
+
+    print(
+        "\033[92m" + "Would you like to rebind the Search/Super(Win key) key to Caps Lock?(NOT RECOMMENDED)" + "\033[0m")
+    while True:
+        if input("\033[94m" + "Type yes to rebind. Press enter to keep old binding" "\033[0m" + "\n") == "yes":
+            print("Search key will be a CAPS LOCK key")
+            rebind_search = True
+        else:
+            print("Search key will be Super/Win key")
+            rebind_search = False
+        break
+
+    print("\033[92m" + "Create image or write to the SD-card/USB-stick directly? (direct write is not yet supported)"
+          + "\033[0m")
+    while True:
+        if input("\033[94m" + "Type 'direct' to use write directly. " +
+                 "Press Enter to create an img file" "\033[0m" + "\n") == "direct":
+            print("USB selected")
+            create_iso = False
+        else:
+            print("Image selected")
+        break
+    print("User input complete")
+    return distro_name, distro_version, distro_link, de_name, use_openbox, username, password, hostname, \
+           rebind_search, create_iso
+
+
+if __name__ == "__main__":
+    print("\033[91m" + "Do not run this file. Run build.py" + "\033[0m")
