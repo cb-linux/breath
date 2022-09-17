9102f23d64d34d2509c726eab18950f62ef9a4e2
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-09-17 15:59:11 +0200

    Remove with bash

--- build.py
+++ build.py
@@ -320,7 +320,7 @@ def post_extract(username: str, password: str, hostname: str, distro_name: str, 
     cpfile("/etc/resolv.conf", "/mnt/eupnea/etc/resolv.conf")
 
     print("Extracting kernel modules")
-    rmdir("/mnt/eupnea/lib/modules")
+    bash("rm -rf /mnt/eupnea/lib/modules/*")  # delete old modules
 
     # modules tar contains /lib/modules, so it's extracted to / and --skip-old-files is used to prevent overwriting
     # other files in /lib
