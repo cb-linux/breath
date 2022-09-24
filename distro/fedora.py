1b79cb5d29eb7b87b868e1713c33fe023132aa8e
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-09-24 17:15:30 +0200

    Remove chrome-gnome-shell from gnome fedora

--- distro/fedora.py
+++ distro/fedora.py
@@ -27,7 +27,7 @@ def config(de_name: str, distro_version: str, root_partuuid: str, verbose_var: b
             print("Installing gnome")
             # As gnome is not preinstalled on the cloud version, we need to install it "manually"
             chroot(
-                "dnf install -y @base-x gnome-shell gnome-terminal nautilus firefox chrome-gnome-shell gnome-tweaks " +
+                "dnf install -y @base-x gnome-shell gnome-terminal nautilus firefox gnome-tweaks " +
                 "@development-tools gnome-terminal-nautilus xdg-user-dirs xdg-user-dirs-gtk gnome-calculator " +
                 "gnome-system-monitor gedit file-roller gdm gnome-initial-setup")
             chroot("systemctl enable gdm.service")
