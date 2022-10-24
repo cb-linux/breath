73d7fd251a433e5c9cb7f8b78077bc1b88c36e6a
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-10-24 21:25:29 +0200

    Fix referenced before assignment error

--- build.py
+++ build.py
@@ -633,7 +633,7 @@ def start_build(verbose: bool, local_path, kernel_type: str, dev_release: bool, 
             with open("/sys/devices/virtual/dmi/id/product_name", "r") as file:
                 product_name = file.read().strip()
         except FileNotFoundError:  # WSL doesnt have dmi data
-            pass
+            product_name = ""
         # TODO: Fix shrinking on Crostini
         if not product_name == "crosvm" and not no_shrink:
             # Shrink image to actual size
