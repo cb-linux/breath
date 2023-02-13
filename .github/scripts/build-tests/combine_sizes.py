0dcd5c6644a1e646b30a607e1e377433110e624c
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-02-13 22:06:45 +0100

    Use old values if 0 and calculate average size

--- .github/scripts/build-tests/combine_sizes.py
+++ .github/scripts/build-tests/combine_sizes.py
@@ -9,7 +9,26 @@ if __name__ == "__main__":
         with open(file) as f:
             data = json.load(f)
         distro_name = file.split("/")[2][6:-5]
+        # The latter scripts require an underscore at the end of the distro name
+        # -> add it to distros that don't have subversions
+        if not distro_name.__contains__("_"):
+            distro_name = f"{distro_name}_"
         all_sizes[distro_name] = data
+
+    # Sometimes the builder script fails due to a network error -> the size is 0 -> replace with old size
+    # open old sizes file
+    with open("os_sizes.json", "r") as f:
+        old_sizes = json.load(f)
+    for distro in all_sizes:
+        for key in all_sizes[distro]:
+            if all_sizes[distro][key] == 0:
+                all_sizes[distro][key] = old_sizes[distro][key]
+
+    # Calculate average sizes
+    for distro in all_sizes:
+        for key in all_sizes[distro]:
+            # add average size to dict under key "average"
+            all_sizes[distro]["average"] = sum(all_sizes[distro][key]) / len(all_sizes[distro][key])
 
     with open("os_sizes.json", "w") as f:
         json.dump(all_sizes, f)
