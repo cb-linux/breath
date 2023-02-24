ca2bf3cf2eb70e6b41ceef2e3d2dc17b97907cbc
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-02-24 16:52:57 +0100

    Test Fedora 38 too

--- .github/scripts/build-tests/combine_sizes.py
+++ .github/scripts/build-tests/combine_sizes.py
@@ -33,6 +33,7 @@ if __name__ == "__main__":
     # Calculate average sizes for distros with multiple versions
     all_sizes["ubuntu_average"] = round(
         (all_sizes["ubuntu_22.04"]["average"] + all_sizes["ubuntu_22.10"]["average"]) / 2, 1)
+    all_sizes["fedora_average"] = round((all_sizes["fedora_37"]["average"] + all_sizes["fedora_38"]["average"]) / 2, 1)
 
     with open("os_sizes.json", "w") as f:
         json.dump(all_sizes, f)
--- .github/scripts/build-tests/fedora-test.py
+++ .github/scripts/build-tests/fedora-test.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     testing_dict = {
         "distro_name": "fedora",
-        "distro_version": "37",
+        "distro_version": "",
         "de_name": "",
         "username": "localuser",
         "password": "test",
@@ -29,34 +29,36 @@ if __name__ == "__main__":
     size_dict = {}
 
     # Start testing
-    for de_name in available_des:
-        testing_dict["de_name"] = de_name
-        print_header(f"Testing Fedora + {de_name}")
-        try:
-            build.start_build(verbose=True, local_path=None, dev_release=False, build_options=testing_dict,
-                              no_download_progress=True)
-            # calculate shrunk image size in gb and round it to 2 decimal places
-            size_dict[de_name] = round(Path("./depthboot.img").stat().st_size / 1073741824, 1)
-        except Exception as e:
-            print_error(str(e))
-            print_error(f"Failed to build Fedora + {de_name}")
-            failed_distros.append(de_name)
-            size_dict[de_name] = 0
-        except SystemExit:
-            print_error(f"Failed to build Fedora + {de_name}, retrying")
+    for version in ["37", "38"]:
+        testing_dict["distro_version"] = version
+        for de_name in available_des:
+            testing_dict["de_name"] = de_name
+            print_header(f"Testing Fedora + {de_name}")
             try:
                 build.start_build(verbose=True, local_path=None, dev_release=False, build_options=testing_dict,
                                   no_download_progress=True)
                 # calculate shrunk image size in gb and round it to 2 decimal places
                 size_dict[de_name] = round(Path("./depthboot.img").stat().st_size / 1073741824, 1)
-            except (Exception, SystemExit) as e:
+            except Exception as e:
                 print_error(str(e))
-                print_error(f"Failed twice to build Fedora + {de_name}")
+                print_error(f"Failed to build Fedora + {de_name}")
                 failed_distros.append(de_name)
                 size_dict[de_name] = 0
+            except SystemExit:
+                print_error(f"Failed to build Fedora + {de_name}, retrying")
+                try:
+                    build.start_build(verbose=True, local_path=None, dev_release=False, build_options=testing_dict,
+                                      no_download_progress=True)
+                    # calculate shrunk image size in gb and round it to 2 decimal places
+                    size_dict[de_name] = round(Path("./depthboot.img").stat().st_size / 1073741824, 1)
+                except (Exception, SystemExit) as e:
+                    print_error(str(e))
+                    print_error(f"Failed twice to build Fedora + {de_name}")
+                    failed_distros.append(de_name)
+                    size_dict[de_name] = 0
 
-    with open("results_fedora_37.txt", "w") as f:
-        f.write(str(failed_distros))
+        with open(f"results_fedora_{version}.txt", "w") as f:
+            f.write(str(failed_distros))
 
-    with open("sizes_fedora_37.json", "w") as file:
-        json.dump(size_dict, file)
+        with open(f"sizes_fedora_{version}.json", "w") as file:
+            json.dump(size_dict, file)
